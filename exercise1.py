2ca6f1807f080c0f259efa75f5b68fd33485c07d
sahanavishwanath <sahanav@uw.edu>
2019-02-26 15:51:08 -0800

    Changes to ex1

--- exercise1.py
+++ exercise1.py
@@ -5,7 +5,11 @@ import dash_html_components as html
 
 # static data
 weekday_in_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-counts_in_order = [160613, 154225, 155175, 150819, 146014, 215725, 203483]
+total_usage = [160613, 154225, 155175, 150819, 146014, 215725, 203483]
+ped_n = [26884, 26444, 25876, 24368, 23403, 36894, 32792]
+ped_s = [28686, 27520, 27224, 25846, 24900, 37808, 34792]
+bike_n = [52401, 49449, 50209, 49692, 47971, 69437, 67752]
+bike_s = [52642, 50812, 51866, 50913, 49740, 71586, 68147]
 # TODO: working on this file to add more codes...
 
 # initialize Dash environment
@@ -28,7 +32,11 @@ app.layout = html.Div(children=[
             # configure the data
             'data': [
                 # set x to be weekday, and y to be the counts. We use bars to represent our data.
-                {'x': weekday_in_order, 'y': counts_in_order, 'type': 'bar', 'name': 'Total'},
+                {'x': weekday_in_order, 'y': total_usage, 'type': 'bar', 'name': 'Total'},
+                {'x': weekday_in_order, 'y': ped_n, 'type': 'bar', 'name': 'Ped N'},
+                {'x': weekday_in_order, 'y': ped_s, 'type': 'bar', 'name': 'Ped S'},
+                {'x': weekday_in_order, 'y': bike_n, 'type': 'bar', 'name': 'Bike N'},
+                {'x': weekday_in_order, 'y': bike_s, 'type': 'bar', 'name': 'Bike S'},
 
             ],
             # configure the layout of the visualization --
@@ -43,6 +51,3 @@ app.layout = html.Div(children=[
 if __name__ == '__main__':
     # start the Dash app
     app.run_server(debug=True)
-
-
-
