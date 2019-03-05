2d6c7965f68e3895ff6f5bc29f86d9e09398065f
sahanavishwanath <sahanav@uw.edu>
2019-03-05 15:47:14 -0800

    Editing ex2

--- exercise2.py
+++ exercise2.py
@@ -9,8 +9,13 @@ import pandas as pd
 app = dash.Dash(__name__, static_folder='static')
 df = pd.read_csv('static/Pokemon.csv')
 
-generations = df.groupby('Generation')
 legendary = df[df['Legendary'] == True]
+gen1_leg = legendary[legendary['Generation'] == 1]
+gen2_leg = legendary[legendary['Generation'] == 2]
+gen3_leg = legendary[legendary['Generation'] == 3]
+gen4_leg = legendary[legendary['Generation'] == 4]
+gen5_leg = legendary[legendary['Generation'] == 5]
+gen6_leg = legendary[legendary['Generation'] == 6]
 
 # set layout of the page
 app.layout = html.Div(children=[
@@ -32,18 +37,68 @@ app.layout = html.Div(children=[
                 # This is how we define a scatter plot. Note that it also uses "go.Scatter",
                 # but with the mode to be only "markers"
                 go.Scatter(
-                    x=legendary['Attack'],
-                    y=legendary['Defense'],
+                    x=gen1_leg['Attack'],
+                    y=gen1_leg['Defense'],
                     mode='markers',
-                    text=legendary['Name'],  # This line sets the vehicle name as the points' labels.
+                    text=gen1_leg['Name'],  # This line sets the vehicle name as the points' labels.
                     marker={
-                        'size': 5,
-                        'opacity': 0.8  # By making the points a bit transparent, it can alleviate the occlusion issue
+                        'size': 15,
+                        'opacity': 0.6  # By making the points a bit transparent, it can alleviate the occlusion issue
+                    }
+                ),
+                go.Scatter(
+                    x=gen2_leg['Attack'],
+                    y=gen2_leg['Defense'],
+                    mode='markers',
+                    text=gen2_leg['Name'],  # This line sets the vehicle name as the points' labels.
+                    marker={
+                        'size': 15,
+                        'opacity': 0.6  # By making the points a bit transparent, it can alleviate the occlusion issue
+                    }
+                ),
+                go.Scatter(
+                    x=gen3_leg['Attack'],
+                    y=gen3_leg['Defense'],
+                    mode='markers',
+                    text=gen3_leg['Name'],  # This line sets the vehicle name as the points' labels.
+                    marker={
+                        'size': 15,
+                        'opacity': 0.6  # By making the points a bit transparent, it can alleviate the occlusion issue
+                    }
+                ),
+                go.Scatter(
+                    x=gen4_leg['Attack'],
+                    y=gen4_leg['Defense'],
+                    mode='markers',
+                    text=gen4_leg['Name'],  # This line sets the vehicle name as the points' labels.
+                    marker={
+                        'size': 15,
+                        'opacity': 0.6  # By making the points a bit transparent, it can alleviate the occlusion issue
+                    }
+                ),
+                go.Scatter(
+                    x=gen5_leg['Attack'],
+                    y=gen5_leg['Defense'],
+                    mode='markers',
+                    text=gen5_leg['Name'],  # This line sets the vehicle name as the points' labels.
+                    marker={
+                        'size': 15,
+                        'opacity': 0.6  # By making the points a bit transparent, it can alleviate the occlusion issue
+                    }
+                ),
+                go.Scatter(
+                    x=gen6_leg['Attack'],
+                    y=gen6_leg['Defense'],
+                    mode='markers',
+                    text=gen6_leg['Name'],  # This line sets the vehicle name as the points' labels.
+                    marker={
+                        'size': 15,
+                        'opacity': 0.6  # By making the points a bit transparent, it can alleviate the occlusion issue
                     }
                 )
             ],
             'layout': {
-                'title': 'Pokemon Attack vs. Defense',
+                'title': 'Legendary Pokemon Attack vs. Defense',
                 # It is always a good practice to have axis labels.
                 # This is especially important in this case as the numbers are not trivial
                 'xaxis': {'title': 'Attack'},
