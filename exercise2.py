fb9970fe524a18ed23cc17549c37cbcffa6bb976
sahanavishwanath <sahanav@uw.edu>
2019-03-05 14:31:31 -0800

    Testing ex1 #2

--- exercise2.py
+++ exercise2.py
@@ -1,57 +1,57 @@
-# -*- coding: utf-8 -*-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objs as go
-import pandas as pd
-
-# initialize Dash app and initialize the static folder
-app = dash.Dash(__name__, static_folder='static')
-df = pd.read_csv('static/Pokemon.csv')
-
-generations = df.groupby('Generation')
-legendary = df[['Legendary'] == True]
-
-# set layout of the page
-app.layout = html.Div(children=[
-
-    # set the page heading
-    html.H1(children='Scatter Plot'),
-
-    # set the description underneath the heading
-    html.Div(children='''
-        A demo to show a scatter plot.
-    '''),
-
-    # append the visualization to the page
-    dcc.Graph(
-        id='ex2-graph',
-        figure={
-            # configure the data
-            'data': [
-                # This is how we define a scatter plot. Note that it also uses "go.Scatter",
-                # but with the mode to be only "markers"
-                go.Scatter(
-                    x=generations['Attack'],
-                    y=generations['Defense'],
-                    mode='markers',
-                    text=generations['Name'],  # This line sets the vehicle name as the points' labels.
-                    marker={
-                        'size': 10,
-                        'opacity': 0.8  # By making the points a bit transparent, it can alleviate the occlusion issue
-                    }
-                )
-            ],
-            'layout': {
-                'title': 'Pokemon Attack vs. Defense',
-                # It is always a good practice to have axis labels.
-                # This is especially important in this case as the numbers are not trivial
-                'xaxis': {'title': 'Attack'},
-                'yaxis': {'title': 'Defense'},
-            }
-        }
-    )
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# # -*- coding: utf-8 -*-
+# import dash
+# import dash_core_components as dcc
+# import dash_html_components as html
+# import plotly.graph_objs as go
+# import pandas as pd
+#
+# # initialize Dash app and initialize the static folder
+# app = dash.Dash(__name__, static_folder='static')
+# df = pd.read_csv('static/Pokemon.csv')
+#
+# generations = df.groupby('Generation')
+# legendary = df[['Legendary'] == True]
+#
+# # set layout of the page
+# app.layout = html.Div(children=[
+#
+#     # set the page heading
+#     html.H1(children='Scatter Plot'),
+#
+#     # set the description underneath the heading
+#     html.Div(children='''
+#         A demo to show a scatter plot.
+#     '''),
+#
+#     # append the visualization to the page
+#     dcc.Graph(
+#         id='ex2-graph',
+#         figure={
+#             # configure the data
+#             'data': [
+#                 # This is how we define a scatter plot. Note that it also uses "go.Scatter",
+#                 # but with the mode to be only "markers"
+#                 go.Scatter(
+#                     x=generations['Attack'],
+#                     y=generations['Defense'],
+#                     mode='markers',
+#                     text=generations['Name'],  # This line sets the vehicle name as the points' labels.
+#                     marker={
+#                         'size': 10,
+#                         'opacity': 0.8  # By making the points a bit transparent, it can alleviate the occlusion issue
+#                     }
+#                 )
+#             ],
+#             'layout': {
+#                 'title': 'Pokemon Attack vs. Defense',
+#                 # It is always a good practice to have axis labels.
+#                 # This is especially important in this case as the numbers are not trivial
+#                 'xaxis': {'title': 'Attack'},
+#                 'yaxis': {'title': 'Defense'},
+#             }
+#         }
+#     )
+# ])
+#
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
