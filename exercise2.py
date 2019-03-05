28acacf275ef2360d19e92f9ea662d56be15507e
sahanavishwanath <sahanav@uw.edu>
2019-03-05 14:10:24 -0800

    More debugging

--- exercise2.py
+++ exercise2.py
@@ -10,6 +10,7 @@ app = dash.Dash(__name__, static_folder='static')
 df = pd.read_csv('static/Pokemon.csv')
 
 generations = df.groupby('Generation')
+legendary = df[['Legendary'] == True]
 
 # set layout of the page
 app.layout = html.Div(children=[
