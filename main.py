b77e9bfff5b6a5646b11e86ef21bb7a587c74999
sahanavishwanath <sahanav@uw.edu>
2019-03-05 16:25:48 -0800

    Testing displays #2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 # Note: import the app of what you are working on
-#from initial_demo import app
+# from initial_demo import app
 from exercise1 import app1
 from exercise2 import app2
 
-server = app.server
+server1 = app1.server
+server2 = app2.server
 
 if __name__ == '__main__':
     # start the Dash app
